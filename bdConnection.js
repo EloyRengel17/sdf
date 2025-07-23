34e2b09abdcbbfc0d20d06f89c1f6009dd9e8d35
Eloy Rengel <eloypanita@gmail.com>
2025-07-23 00:01:23 -0400

    Conexión a PostgreSQL con Railway

--- bdConnection.js
+++ bdConnection.js
@@ -1,10 +1,11 @@
 import {Pool} from 'pg';
 
 const pool= new Pool({
-    user: "postgres",
-    password: "admin",
-    host: "localhost",
-    database:"gimnasioVac25",
-    port: 5432,
+    user: postgres,
+    password: acwksOsQZStBkjmENXkEXsXJKNycHeyy,
+    host: maglev.proxy.rlwy.net,
+    database: railway,
+    port: 23575,
+    ssl: { rejectUnauthorized: false }
 })
 export default pool;
