5ca8935520dec5e455e14964a16efb484d49de3c
Eloy Rengel <eloypanita@gmail.com>
2025-07-23 01:36:54 -0400

    Conexión a PostgreSQL con Railway

--- bdConnection.js
+++ bdConnection.js
@@ -1,11 +1,12 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 
-const pool= new Pool({
-    user: "postgres",
-    password: xuBJKbJUZHGGfFZHkGLcmvolwWKbCEeY,
-    host: gondola.proxy.rlwy.net,
-    database: railway,
-    port: 36019,
-    ssl: { rejectUnauthorized: false }
-})
+const pool = new Pool({
+  user: "postgres",
+  password: "xuBJKbJUZHGGfFZHkGLcmvolwWKbCEeY",
+  host: "gondola.proxy.rlwy.net",
+  database: "railway",
+  port: 36019,
+  ssl: { rejectUnauthorized: false }
+});
+
 export default pool;
